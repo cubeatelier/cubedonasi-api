ad02bd0edf650850441e908f726fe1ec5e81a297
CUBE ATELIER <alinuha1104@gmail.com>
2026-04-27 01:45:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -32,20 +32,5 @@ app.get("/donations", (req, res) => {
   res.json({ latestDonation: latestDonation });
 });
 
-// ==========================================
-// ENDPOINT 3: JALUR TES RAHASIA VIA BROWSER
-// ==========================================
-app.get("/tes-donasi", (req, res) => {
-  // Kita buat data donasi bohongan sebesar Rp 50.000 agar Cinematic-nya nyala
-  latestDonation = {
-    id: Date.now().toString(),
-    name: "Sultan Tester",
-    amount: 50000, 
-    message: "Halo CUBE CLUB! Ini tes efek layarnya!"
-  };
-  
-  res.send("<h1>✅ Sinyal donasi Rp 50.000 sukses ditembakkan!</h1><p>Sekarang cepat buka layar Roblox Studio kamu, dalam 10 detik efeknya akan muncul!</p>");
-});
-
 // WAJIB UNTUK VERCEL
 module.exports = app;
